d87da17e7b1a7ea56faaef0deca18e93de0613b0
Hasan Amirov <a.amirovv@mail.ru>
2020-12-16 19:08:35 +0300

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <span :class="{done: todo.completed}">
+      <input type="checkbox" @click="todo.completed = !todo.completed">
+      <strong>{{ index + 1 }}</strong>
+      {{ todo.title}}
+    </span>
+    <button class="btn" @click="$emit('remove-todo', todo.id)">&times;</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+    index: Number
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  li {
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: .5rem 2rem;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    margin-right: 1rem;
+  }
+
+  .btn {
+    background: red;
+    color: #fff;
+    border-radius: 50%;
+    font-weight: bold;
+    outline: none;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home pages</h2>
+        <hr>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
